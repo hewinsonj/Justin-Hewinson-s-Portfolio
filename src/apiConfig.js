583b386ffea941d483d0ad6ef326335833ba049e
hewinsonj <hewinsonj@gmail.com>
2024-04-05 00:20:33 -0400

    changed produdction url in apiConfig file

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -2,7 +2,7 @@ let apiUrl
 const apiUrls = {
     // YOU MUST CHANGE PRODUCTION URL WHEN DEPLOYING
 	production: 'https://hewinsonportfolioapi.fly.dev/',
-	development: 'http://localhost:8000',
+	// development: 'http://localhost:8000',
 }
 
 if (window.location.hostname === 'localhost') {
